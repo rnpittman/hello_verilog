8f92285303c48c2cc647195d9f75c92c612ac012
rnpittman <rnpittman@live.com>
2014-09-09 10:59:50 -0700

    Create counter.v
    test Verilog counter module

---
+++ counter.v
@@ -0,0 +1,25 @@
+module counter(
+  input clk,
+  input reset,
+  input [7:0] in,
+  output reg [7:0] out
+);
+
+intial
+begin
+  out <= 8'b0;
+end
+
+always@(posedge clk)
+begin
+  if (reset)
+  begin
+    out <= in;
+  end
+  else
+  begin
+    out <= out+1;
+  end
+end
+
+endmodule
